88018e55978e7a36c34b641ad1580224d532329b
NILAR HTWE <iamstampking@gmail.com>
2023-11-01 16:29:48 +0700

    createworkcategories

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { createServer } = require("http");
 const workRoute = require("./routes/work-route");
 const authRoute = require("./routes/auth-route");
 const adminRoute = require('./routes/admin-route')
+const categoryRoute = require('./routes/category-route')
 
 const app = express();
 const server = createServer(app);
@@ -14,9 +15,10 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.use('/auth',authRoute)
-app.use('/work',workRoute)
-app.use('/admin',adminRoute)
+app.use('/auth', authRoute)
+app.use('/work', workRoute)
+app.use('/admin', adminRoute)
+// app.use('/category', categoryRoute)
 
 
 const PORT = process.env.PORT || 8080;
--- src/controllers/work-controller.js
+++ src/controllers/work-controller.js
@@ -1,3 +1,4 @@
+const { date } = require("joi");
 const { STATUS_WORK_CREATE } = require("../configs/constants");
 const prisma = require("../models/prisma");
 
@@ -49,3 +50,36 @@ exports.deleteWork = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.createworkCategories = async (req, res, next) => {
+  try {
+    const value = req.body;
+    console.log(value);
+    const workCategories = await prisma.workCategories.create({
+      data: {
+        category: value.category
+      }
+    })
+    res.status(201).json({ workCategories })
+  } catch (error) {
+    next(error)
+  }
+}
+
+exports.getAllCategories = async (req, res, next) => {
+  try {
+    const { value } = req.params;
+    const allCategories = await prisma.workCategories.findMany({
+      where: {
+        data: value
+      }
+    })
+    console.log(value);
+    res.status(201).json({ allCategories });
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+
