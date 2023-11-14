d88613b90bc824e01500bd68fa33db228aad6637
HeartLezz7 <heartchantapath@gmail.com>
2023-11-14 15:45:37 +0700

    finish update status work

--- src/controllers/work-controller.js
+++ src/controllers/work-controller.js
@@ -267,9 +267,17 @@ exports.rejectwork = async (req, res, next) => {
 
 exports.updateStatusWork = async (req, res, next) => {
   try {
-    const status = req.body.status;
-    console.log(status);
-    res.status(201).json({ status });
+    const { workId, workStatus } = req.body;
+    console.log(workStatus, "AAA");
+    const foundWork = await prisma.work.findFirst({ where: { id: +workId } });
+    if (!foundWork) {
+      createError("not found work", 401);
+    }
+    const updateStatus = await prisma.work.update({
+      where: { id: foundWork.id },
+      data: { statusWork: workStatus },
+    });
+    res.status(201).json({ updateStatus });
   } catch (err) {
     next(err);
   }
